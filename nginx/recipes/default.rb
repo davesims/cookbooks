cd77bcf5872ca6b3ec66787c2aae630508cea3a1
Dave Sims <davsims@gmail.com>
2013-12-21 23:43:30 -0600

    setting up nginx

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -1,10 +1,10 @@
-package "nginx"
+package 'nginx'
 
-service "nginx" do
+service 'nginx' do
   supports :status => true, :restart => true, :reload => true
   action [:enable, :start]
 end
 
 template "/etc/nginx/nginx.conf" do
-  notifies :reload, "service[nginx]"
+
 end
